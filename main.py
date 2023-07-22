2216cba272500b0b7552fee85ea00a035397e47f
jianxing-ma <jianxing.ma@tlgcohort.com>
2023-07-22 01:31:26 -0400

    added welcome message

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from map import *
 
 
 def main():
+    print(read_text_file("welcome_message.txt"))
+    input()
+
     curses.wrapper(execute)
 
     curses.endwin()
@@ -30,13 +33,11 @@ def navigate_map(stdscr, map_coords):
         stdscr.addch(y, x, "X", BACK_COLORS[5])
         stdscr.refresh()
 
-        # temp_y, temp_x = stdscr.getyx()
         if (x, y) in map_coords["zombies"]:
             encounter_zombies(stdscr)
             map_coords["zombies"].remove((x, y))
         if (x, y) in map_coords["potions"]:
-            pass
-        # x, y = temp_x, temp_y
+            encounter_potion(stdscr)
 
         # Wait for user input (arrow keys)
         key = stdscr.getch()
@@ -69,8 +70,13 @@ def navigate_map(stdscr, map_coords):
 def encounter_zombies(stdscr):
     messages = read_text_messages("zombie_messages.txt")
     stdscr.addstr(28, 1, " " * 105)
-    stdscr.addstr(28, 10, messages[random.randrange(len(messages))])
+    stdscr.addstr(28, 10, messages[random.randrange(len(messages))], FORE_COLORS[0])
     stdscr.refresh()
+
+
+def encounter_potion(stdscr):
+    stdscr.addstr(28, 1, " " * 105)
+    stdscr.addstr(28, 40, "You retrieved a potion!", FORE_COLORS[1])
 
 
 if __name__ == "__main__":
